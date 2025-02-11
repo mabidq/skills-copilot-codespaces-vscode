1fd5a6f00401027765fce81139efacadde726d77
mabidq <mabid@pekininsurance.com>
2025-02-11 17:13:53 +0000

    Copilot third commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+    skills.style.display = "block";
+    projects.style.display = "none";
+    about.style.display = "none";
+    contact.style.display = "none";
+}
